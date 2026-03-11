1ab1e565a4954d48c2b2904f046e46205770699a
mars.maya.g@gmail.com <mars.maya.g@gmail.com>
2026-03-11 16:12:43 +0800

    ?

--- run_solve_alignment_from_stars.bat
+++ run_solve_alignment_from_stars.bat
@@ -1,3 +1,3 @@
 @echo off
-python "D:\github\misaligned_fits\solve_alignment_from_stars.py" --a-stars "%~1" --b-stars "%~2" --out "%~3"
+python "D:\github\misaligned_fits\solve_alignment_from_stars.py" --a-stars "D:/github/test_flow_data/stars/K024-6.stars.npz" --b-stars "D:/github/test_flow_data/stars/GY1_K024-6_NoFilter_60S_Bin2_UTC20260303_162922_-29.9C_stars.npz" --out "D:/github/test_flow_data/stars/GY1_K024-6_NoFilter_60S_Bin2_UTC20260303_162922_-29.9C_align.npz"
 exit /b %ERRORLEVEL%
