edb3b47daae847283794be20dcd4a75af711e43a
mars.maya.g@gmail.com <mars.maya.g@gmail.com>
2026-03-11 13:09:01 +0800

    ？

--- run_export_stars.bat
+++ run_export_stars.bat
@@ -1,3 +1,3 @@
 @echo off
-python "D:/github/misaligned_fits/export_fits_stars.py" --fits "D:/github/test_flow_data/temp/gy1/K024-6.fits" --out "D:/github/test_flow_data/temp/gy1"
+python "D:/github/misaligned_fits/export_fits_stars.py" --fits "D:/github/test_flow_data/temp/gy1/K024-6.fits" --out "D:/github/test_flow_data/temp/gy1/K024-6.stars.npz"
 exit /b %ERRORLEVEL%
