12a84f54b1ed7f9efb2a3349df0b0ee229947a4e
mars.maya.g@gmail.com <mars.maya.g@gmail.com>
2026-03-12 14:30:03 +0800

    加个参数

--- run_rank_variable_candidates.bat
+++ run_rank_variable_candidates.bat
@@ -1,3 +1,3 @@
 @echo off
-python "D:\github\misaligned_fits\rank_variable_candidates.py" --base "D:/align" --ref "D:/align/gy3_K043-8.fits" --out-csv "D:/align/output/variable_candidates_rank.csv" --out-png "D:/align/output/variable_candidates_rank.png"
+python "D:\github\misaligned_fits\rank_variable_candidates.py" --base "D:/align" --ref "D:/align/gy3_K043-8.fits" --out-csv "D:/align/output/variable_candidates_rank.csv" --out-png "D:/align/output/variable_candidates_rank.png" --min-observations 2
 exit /b %ERRORLEVEL%
