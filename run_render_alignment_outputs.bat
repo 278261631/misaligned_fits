86820360499fda0aa5451e94258d18866fc79be7
mars.maya.g@gmail.com <mars.maya.g@gmail.com>
2026-03-12 11:05:36 +0800

    效果不好

--- run_render_alignment_outputs.bat
+++ run_render_alignment_outputs.bat
@@ -1,3 +1,3 @@
 @echo off
-python "D:\github\misaligned_fits\render_alignment_outputs.py" --a "%~1" --b "%~2" --align "%~3" --outdir "%~4"
+python "D:\github\misaligned_fits\render_alignment_outputs.py" --a "D:/align/gy3_K043-8.fits" --b "D:/align/GY3_K043-8_UTC20260310_163817_-25C_.fit" --align "D:/align/GY3_K043-8_UTC20260310_163817_-25C_.align.npz" --outdir "D:/align/output/"
 exit /b %ERRORLEVEL%
